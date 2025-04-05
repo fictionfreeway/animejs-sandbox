c4ae14e7753f89d39fc42af1399cfa8046986b13
William Watlington <fictionfreeway@gmail.com>
2025-04-05 14:21:01 -0400

    add animejs dependency and implement animation in sandbox component

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { animate } from 'animejs';
 
 @Component({
   selector: 'app-sandbox',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sandbox.component.html',
   styleUrl: './sandbox.component.scss'
 })
-export class SandboxComponent {
+export class SandboxComponent implements AfterViewInit {
 
+  ngAfterViewInit() {
+
+    animate(
+      '#test-header',  
+      {
+        rotate: {
+          from: '0turn',
+          to: '.5turn',
+          delay: 0 
+        },
+        loopDelay: 1000,
+        loop: true,
+        ease: 'outElastic'
+      }
+    );
+  }
 }
